a87bb4038fcb09d02705027b9b78487b9fd98948
NguyenTanPil <traila1008@gmail.com>
2021-11-20 12:58:58 +0700

    refactor transposition cipher

--- ciphers/transposition.py
+++ ciphers/transposition.py
@@ -8,35 +8,64 @@ class Transposition:
         # split array has length columns
         return [seq[i:i + length] for i in range(0, len(seq), length)]
 
+    def encode(self, message):
+        # remember indexs
+        indexs_remember = [i for i, char in enumerate(message) if char == ' ']
 
-    def transition_code(self,message, encrypt=True):
         # remove spacing
-        try:
-            new_message = message.replace(' ', '')
-        except AttributeError:
-            new_message = message
+        new_message = message.replace(' ', '')
+
         # convert string to dict
         order = {
-            int(val): num for num, val in enumerate(self.key)
+            int(num): (int(val) - 1) for num, val in enumerate(self.key)
         }
+        length = len(self.key)
         result = ''
-        length = len(self.key) if encrypt else int(len(new_message) / len(self.key))
+        matrix = self.split_array_by_len(new_message, length)
 
         for column in sorted(order.keys()):
-            for row in self.split_array_by_len(message, length):
+            for row in matrix:
                 try:
                     result += row[order[column]]
                 except IndexError:
                     continue
+        
+        # add space remember
+        for index in indexs_remember:
+            result = result[:index] + ' ' + result[index:]
 
         return result
 
 
-    def encode(self,message):
-        return self.transition_code(message)
+    def decode(self, message):
+        # remember indexs
+        indexs_remember = [i for i, char in enumerate(message) if char == ' ']
 
+        # remove spacing
+        new_message = message.replace(' ', '')
 
-    def decode(self,message):
-        return self.transition_code(message, False)
+        # convert string to dict
+        order = {
+            int(val): (int(num)) for num, val in enumerate(self.key)
+        }
+        length = int(len(new_message) / len(self.key)) + (len(new_message) % len(self.key) > 0)
+        matrix = self.split_array_by_len(new_message, length)
 
+        result = ''
+        
+        for i in range(length):
+            part = ''
+            for column in sorted(order.keys()):
+                matrix_index = order[column]
+                try:
+                    part += matrix[matrix_index][i]
+                except IndexError:
+                    continue
 
+            result += part
+        
+        # add space remember
+        for index in indexs_remember:
+            result = result[:index] + ' ' + result[index:]
+
+        return result
